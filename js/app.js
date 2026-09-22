4ebf4ed112b3f123528ab890a51a956bed7e1c85
Paavo92 <paavo.salo92@gmail.com>
2026-09-22 15:45:00 +0300

    Refactor question display and answer checking logic

--- js/app.js
+++ js/app.js
@@ -1,6 +1,39 @@
 fetch("data/questions.json")
   .then(response => response.json())
   .then(questions => {
-    document.getElementById("output").innerHTML =
-      `<h2>${questions[0].question}</h2>`;
+
+    const question = questions[0];
+
+    let html = `
+      <h2>${question.question}</h2>
+    `;
+
+    question.options.forEach(option => {
+      html += `
+        <button onclick="checkAnswer('${option}', '${question.correctAnswer}')">
+          ${option}
+        </button>
+        <br><br>
+      `;
+    });
+
+    html += `
+      <div id="result"></div>
+    `;
+
+    document.getElementById("output").innerHTML = html;
   });
+
+function checkAnswer(selected, correct) {
+
+  const result = document.getElementById("result");
+
+  if (selected === correct) {
+    result.innerHTML = "<h3 style='color:green;'>Correct!</h3>";
+  } else {
+    result.innerHTML = `
+      <h3 style='color:red;'>Incorrect!</h3>
+      <p>Correct answer: ${correct}</p>
+    `;
+  }
+}
