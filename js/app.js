8ee5a1440eebcbfe7dc3c63d4105c92a6122518f
Paavo92 <paavo.salo92@gmail.com>
2026-09-22 16:01:07 +0300

    Add onclick handler for answer buttons

--- js/app.js
+++ js/app.js
@@ -7,9 +7,29 @@ fetch("data/questions.json")
     let html = "<h2>" + question.question + "</h2>";
 
     question.options.forEach(option => {
-      html += "<button>" + option + "</button><br><br>";
+      html +=
+        "<button onclick=\"checkAnswer('" +
+        option +
+        "')\">" +
+        option +
+        "</button><br><br>";
     });
 
+    html += "<div id='result'></div>";
+
     document.getElementById("output").innerHTML = html;
+
+    window.correctAnswer = question.correctAnswer;
   });
-``
+
+function checkAnswer(selected) {
+
+  if (selected === window.correctAnswer) {
+    document.getElementById("result").innerHTML =
+      "<h3 style='color:green'>Correct!</h3>";
+  } else {
+    document.getElementById("result").innerHTML =
+      "<h3 style='color:red'>Incorrect!</h3>" +
+      "<p>Correct answer: " + window.correctAnswer + "</p>";
+  }
+}
