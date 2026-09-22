491a270a8c8a75ab9319deb0d3ac593ac07a1cdb
Paavo92 <paavo.salo92@gmail.com>
2026-09-22 15:50:40 +0300

    Replace dynamic question fetching with static HTML
    Removed fetch logic and replaced it with static HTML for test question.

--- js/app.js
+++ js/app.js
@@ -1,37 +1,4 @@
-fetch("data/questions.json")
-  .then(response => response.json())
-  .then(questions => {
-
-    console.log(questions);
-
-    const question = questions[0];
-
-    let html = "<h2>" + question.question + "</h2>";
-
-    for (let i = 0; i < question.options.length; i++) {
-
-      html += "<button onclick=\"checkAnswer('" +
-        question.options[i] +
-        "','" +
-        question.correctAnswer +
-        "')\">";
-
-      html += question.options[i];
-      html += "</button><br><br>";
-    }
-
-    html += "<div id='result'></div>";
-
-    document.getElementById("output").innerHTML = html;
-  });
-
-function checkAnswer(selected, correct) {
-
-  if (selected === correct) {
-    document.getElementById("result").innerHTML =
-      "<h3>Correct!</h3>";
-  } else {
-    document.getElementById("result").innerHTML =
-      "<h3>Incorrect!</h3><p>Correct answer: " + correct + "</p>";
-  }
-}
+document.getElementById("output").innerHTML =
+  "<h2>Test Question</h2>" +
+  "<button>Answer 1</button><br><br>" +
+  "<button>Answer 2</button>";
