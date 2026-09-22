d9cf8673504156dd6079bb0033e9e55b42507d0f
Paavo92 <paavo.salo92@gmail.com>
2026-09-22 15:53:32 +0300

    Fix HTML output formatting in app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
 document.getElementById("output").innerHTML =
-  "<h2>Test Question</h2>" +
-  "<button>Answer 1</button><br><br>" +
-  "<button>Answer 2</button>";
+  <h2>Test Question</h2> +
+  <button>Answer 1</button><br><br> +
+  <button>Answer 2</button>;
